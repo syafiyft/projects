91ce899beedee2bfac127deda319c87f410de0bb
Syafiy <syafiyft@gmail.com>
2025-08-13 11:51:33 +0800

    Add initial variables and update app theme settings

--- assessment_aem/lib/main.dart
+++ assessment_aem/lib/main.dart
@@ -4,6 +4,17 @@ void main() {
   runApp(const MyApp());
 }
 
+String name = "AEM Assessment";
+int number = 1;
+double decimal = 1.1;
+List<dynamic> myList = ["AEM", "Assessment", 123];
+Map<String, dynamic> myMap = {
+  "name": "AEM Assessment",
+  "number": 1,
+  "decimal": 1.1,
+  "myList": ["AEM", "Assessment", 123],
+};
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -28,7 +40,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -86,28 +101,29 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              height: 100.0,
+              width: 100.0,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 0.5),
+                borderRadius: BorderRadius.circular(25.0),
+                color: Colors.blueGrey,
+              ),
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 0.5),
+                borderRadius: BorderRadius.circular(25.0),
+                color: Colors.blueGrey,
+              ),
             ),
           ],
         ),
