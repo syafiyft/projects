44d6d25f0d53f84e214eed78e51206b84186fe25
Syafiy <syafiyft@gmail.com>
2025-08-24 02:49:28 +0800

    Add checkboxes and switches to ProfilePage for user interaction

--- tutorial_flutter/lib/views/pages/profile_page.dart
+++ tutorial_flutter/lib/views/pages/profile_page.dart
@@ -9,11 +9,15 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitched = false;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           TextField(
             controller: controller,
@@ -23,6 +27,42 @@ class _ProfilePageState extends State<ProfilePage> {
             },
           ),
           Text(controller.text),
+          Checkbox(
+            tristate: true,
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          CheckboxListTile(
+            tristate: true,
+            title: Text('Click Me'),
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          Switch(
+            value: isSwitched,
+            onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+          SwitchListTile(
+            title: Text('Click Me'),
+            value: isSwitched,
+            onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
         ],
       ),
     );
