daf247d673d4a9d0414f41f3386935591adbdb97
Syafiy <syafiyft@gmail.com>
2025-08-29 01:04:48 +0800

    card

--- tutorial_flutter/lib/views/pages/home_page.dart
+++ tutorial_flutter/lib/views/pages/home_page.dart
@@ -8,7 +8,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20.0),
-      child: Column(children: [HeroWidget()]),
+      child: Column(
+        children: [
+          HeroWidget(),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsetsGeometry.symmetric(vertical: 20.0),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Basic Layout'),
+                    Text('The description of this'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
