e282d0d5d26bae3fc22a56cadc1e3938ef74e683
Syafiy <syafiyft@gmail.com>
2025-08-28 21:18:13 +0800

    navigator.push

---
+++ tutorial_flutter/lib/views/pages/settings_page.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
+  String? menuItem = 'e1';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings Page')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DropdownButton(
+                value: menuItem,
+                items: [
+                  DropdownMenuItem(value: 'e1', child: Text('Item 1')),
+                  DropdownMenuItem(value: 'e2', child: Text('Item 2')),
+                  DropdownMenuItem(value: 'e3', child: Text('Item 3')),
+                ],
+                onChanged: (String? value) {
+                  setState(() {
+                    menuItem = value;
+                  });
+                },
+              ),
+              TextField(
+                controller: controller,
+                decoration: InputDecoration(border: OutlineInputBorder()),
+                onEditingComplete: () {
+                  setState(() {});
+                },
+              ),
+              Text(controller.text),
+              Checkbox(
+                tristate: true,
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value;
+                  });
+                },
+              ),
+              CheckboxListTile.adaptive(
+                tristate: true,
+                title: Text('Click Me'),
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value;
+                  });
+                },
+              ),
+              Switch(
+                value: isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+              SwitchListTile.adaptive(
+                title: Text('Click Me'),
+                value: isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+              Slider.adaptive(
+                max: 10.0,
+                value: sliderValue,
+                divisions: 10,
+                onChanged: (value) {
+                  setState(() {
+                    sliderValue = value;
+                  });
+                  print(value);
+                },
+              ),
+              InkWell(
+                splashColor: Colors.teal,
+                highlightColor: Colors.red,
+                onTap: () {
+                  print('Image Clicked');
+                },
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  color: Colors.white12,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text('Click Me'),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('Click Me')),
+              OutlinedButton(onPressed: () {}, child: Text('Click Me')),
+              TextButton(onPressed: () {}, child: Text('Click Me')),
+              FilledButton(onPressed: () {}, child: Text('Click Me')),
+              BackButton(),
+              CloseButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- tutorial_flutter/lib/views/widget_tree.dart
+++ tutorial_flutter/lib/views/widget_tree.dart
@@ -1,6 +1,7 @@
 import 'package:assessment_aem/data/notifiers.dart';
 import 'package:assessment_aem/views/pages/home_page.dart';
 import 'package:assessment_aem/views/pages/profile_page.dart';
+import 'package:assessment_aem/views/pages/settings_page.dart';
 import 'package:flutter/material.dart';
 
 import 'widgets/navbar_widget.dart';
@@ -26,6 +27,19 @@ class WidgetTree extends StatelessWidget {
                 return Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode);
               },
             ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SettingsPage();
+                  },
+                ),
+              );
+            },
+            icon: Icon(Icons.settings),
           ),
         ],
       ),
